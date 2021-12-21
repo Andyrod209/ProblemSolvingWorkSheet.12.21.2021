b8b9a61865a4a285876a430ea92adf14c3a84b80
andyrod <andyrod209@gmail.com>
2021-12-21 10:40:09 -0800

    added a user input to capitalize their full name. Now working on how to comress a string of characters.

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ print(normal_string)
 reversed_string = normal_string[::-1]
 print(reversed_string) 
 
+# 2 Capitalize letter 
+user_name = input('What is your full name? ')
+# using title() will make the first letter of each word capitlize
+user_name = user_name.title()
+print(user_name)
+
+#  compress a srting of characters
+user_choice_of_letters = input('Type as many letters as you want... ')
+user_choice_of_letters
