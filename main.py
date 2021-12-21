8d3c5f7d92b6776e35678af17c2aaa9d00828129
andyrod <andyrod209@gmail.com>
2021-12-21 13:03:05 -0800

    Finished compress strings of characters and added notes on how the function works

--- main.py
+++ main.py
@@ -13,4 +13,27 @@ print(user_name)
 
 #  compress a srting of characters
 user_choice_of_letters = input('Type as many letters as you want... ')
-user_choice_of_letters
+# prints the number of times a letter is repeated before it changes to a different letter 
+def compressed(word):
+    index = 0
+    compress = '' 
+    len_of_word = len(word)
+    
+    while index != len_of_word:
+        count = 1
+
+         ####                               checking if both values are true  
+        while (index < len_of_word - 1) and (word[index] == word[index + 1]):
+            # adding 1 to count to count the number of times a word was repeated and adding 1 to index to move to the next index number
+            count = count + 1
+            index = index + 1
+        if count == 1:
+            # adding a letter that was only used once
+            compress += str(word[index])
+        else:
+            compress += str(word[index] + str(count))
+        # index number added by one to grab the next letter
+        index = index + 1 
+    return compress
+
+print(compressed(user_choice_of_letters))
