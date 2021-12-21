7d98f30069c8f21cccc340965b377ff7fbfd40ad
andyrod <andyrod209@gmail.com>
2021-12-21 09:37:17 -0800

    Added a comment on how the operation is working

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # 1 writing a Reverse a string
 normal_string = "Master Cheif"
 print(normal_string)
+# using :: with -1 moves to the back of the string
 reversed_string = normal_string[::-1]
-print(reversed_string)
+print(reversed_string) 
+
