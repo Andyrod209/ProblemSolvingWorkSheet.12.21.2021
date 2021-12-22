e1351eb9089f8f566aff85a438503ac1d9b832bb
andyrod <andyrod209@gmail.com>
2021-12-22 08:52:21 -0800

    build a working happy number function but it only works with two numbers

--- worksheetpart2.py
+++ worksheetpart2.py
@@ -1,39 +1,29 @@
-# # 1 happy number
-# def happy_number(number):
-#     not_one = True 
-#     while not_one is True:
-#         # numbers multiply to the their square root and then add up to each other
-#         index_zero = int(number[0]) * int(number[0])
-#         index_one = int(number[1]) * int(number[1])
-#         total = (index_one) + (index_zero)
-#         # made total a string because i kept on getting an error
-#         total = str(total)
-#         print(total)
-    
-#         if total == 1 or total == 10 or total == 100 or total == 1000 or total == 10000:
-#             print(f'{number} is a happy Number!') 
-
-#         else:
+# 1 happy number
+def happy_number(number):
+    count = 1
+    not_one = True 
+    while not_one is True:
+        index_zero = int(number[0]) * int(number[0])
+        index_one = int(number[1]) * int(number[1])
         
-#             while not_one is True:
-#                 total_index_zero = int(total[0]) * int(total[0])
-#                 total_index_one = int(total[1]) * int(total[1])
-#                 total = total_index_zero + total_index_one
-#                 print(total)
-#                 break
+        total = index_zero + index_one 
+        number = total
+        number = str(number)
+        if number == '1' or number == '10' or number == '100' or number == '1000' or number == '10000':
+            print(f'{user_number} is a happy number!')
+            break
+        elif number == number:
+            count += 1
+            if count == 15:
+                print(f'{user_number} is a sad number')
+                break
 
-#             # added while loop to add numbers that hasn't got to one yet
+        
             
-# user_number = input('pick a number... ')  
-# calling_function = happy_number(user_number)
+user_number = input('pick a number... ')  
+calling_function = happy_number(user_number)
 
 
 
-number = '19'
-not_one = True
-while not_one is True:
-    index_zero = int(number[0]) * int(number[0])
-    index_one = int(number[1]) * int(number[1])
-    total = index_zero + index_one 
-    number = total
-    number = str(number)
+
+    
